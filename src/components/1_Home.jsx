21936dad6b059f52fab1c3a5fc85dfb0643a2ed2
E-moL0587 <kai.pika.025@outlook.com>
2023-10-29 22:21:09 +0900

    First Commit

--- src/components/1_Home.jsx
+++ src/components/1_Home.jsx
@@ -4,8 +4,9 @@ import React from 'react';
 function Home({ onCamera, onAlbum }) {
   return (
     <div>
-      <h2>写真ノート</h2>
-      <img src="./images/book.gif" alt="ノート" style={{ width: '50%' }} /><br/>
+      <br />
+      <h1>phono!</h1>
+      <img src="./images/book.gif" alt="ノート" style={{ width: '75%' }} /><br/>
       <p>文字を抽出してノートに保存します。</p>
       <button onClick={onCamera}>カメラの起動</button>
       <button onClick={onAlbum}>アルバム</button>
--- src/components/2-2_Album.jsx
+++ src/components/2-2_Album.jsx
@@ -5,15 +5,12 @@ import { collection, getDocs, deleteDoc, updateDoc } from 'firebase/firestore';
 import './2-2_Album.css';
 
 function Album({ onBack }) {
-
-  // 初期化
   const [data, setData] = useState([]);
-  const [edit, setEdit] = useState(null);
+  const [selectedPhotoIndex, setSelectedPhotoIndex] = useState(null);
   const [editTex, setEditTex] = useState('');
   const [snapshot, setSnapshot] = useState(null);
   const Space = (text) => { return text.replace(/ /g, ''); };
 
-  // データを取得する
   const AlbumData = async () => {
     const collectionRef = collection(db, 'collection');
     const snap = await getDocs(collectionRef);
@@ -27,47 +24,58 @@ function Album({ onBack }) {
 
   useEffect(() => { AlbumData(); }, []);
 
-  // 編集する
-  const hl_Edit = (index) => { setEdit(index); setEditTex(data[index].field2); };
-
-  // 削除する
-  const hl_Delete = async (index) => {
-    const docToDelete = snapshot.docs[index];
-    if (docToDelete) { await deleteDoc(docToDelete.ref); AlbumData(); }
+  const hl_Edit = (index) => {
+    setSelectedPhotoIndex(index);
+    setEditTex(data[index].field2);
   };
 
-  // 編集を保存する
+  const hl_Delete = async (index) => {
+    const docToDelete = snapshot.docs[index];
+    await deleteDoc(docToDelete.ref);
+    AlbumData();
+    setSelectedPhotoIndex(null);
+  };
+
   const hl_SaveEdit = async (index) => {
     const docToEdit = snapshot.docs[index];
     await updateDoc(docToEdit.ref, { field2: editTex });
-    setEdit(null); AlbumData(); alert('保存されました！');
+    setSelectedPhotoIndex(null);
+    AlbumData();
+    alert('保存されました！');
+  };
+
+  const goBack = () => {
+    setSelectedPhotoIndex(null);
   };
 
   return (
     <div className="album-container">
       <h2>アルバム一覧</h2>
-      <div className="album-grid">
-      {data.map((item, index) => (<div key={index}>
-          <img src={item.field1} alt="Album" style={{ width: '50%' }} />
-
-          {edit === index ? (
-            // 編集モードである場合
-            <div>
-              <input type="text" value={editTex} onChange={(e) => setEditTex(e.target.value)} />
-              <button onClick={() => hl_SaveEdit(index)}>保存</button>
-            </div>
-          ) : (
-            // 編集モードでない場合
-            <div>
-              {Space(item.field2).split('\n').map((line, i) => (<div key={i}>{line}</div>))}<br/>
-              <button onClick={() => hl_Edit(index)}>編集</button>
-              <button onClick={() => hl_Delete(index)}>削除</button>
-            </div>
-          )}<br />
-
-      </div>))}
-      </div>
-      <button onClick={onBack}>戻る</button>
+      {selectedPhotoIndex !== null ? (
+        <div className="photo-detail">
+          <img src={data[selectedPhotoIndex].field1} alt="Album" style={{ width: '50%' }} />
+          <div>
+            {Space(data[selectedPhotoIndex].field2).split('\n').map((line, i) => (
+              <div key={i}>{line}</div>
+            ))}
+          </div>
+          <input type="text" value={editTex} onChange={(e) => setEditTex(e.target.value)} />
+          <button onClick={() => hl_SaveEdit(selectedPhotoIndex)}>保存</button>
+          <button onClick={() => hl_Delete(selectedPhotoIndex)}>削除</button>
+          <button onClick={goBack}>戻る</button>
+        </div>
+      ) : (
+        <div>
+          <div className="album-grid">
+            {data.map((item, index) => (
+              <div key={index} onClick={() => hl_Edit(index)}>
+                <img src={item.field1} alt="Album" style={{ width: '150px', height: '150px' }} />
+              </div>
+            ))}
+          </div><br />
+          <button onClick={onBack}>戻る</button>
+        </div>
+      )}
     </div>
   );
 }
