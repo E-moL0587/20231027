0b5c71f608bcf75417480bd11fa7d9e8d9b48335
na2kera <natsu@keranatsukinoMacBook-Air.local>
2023-10-30 18:26:17 +0900

    反映しました

--- src/components/1_Home.jsx
+++ src/components/1_Home.jsx
@@ -1,5 +1,5 @@
 // ホーム画面
-import React from 'react';
+import React from "react";
 
 function Home({ onCamera, onAlbum }) {
   return (
@@ -8,7 +8,9 @@ function Home({ onCamera, onAlbum }) {
       <h1>phono!</h1>
       <p>kaito反映されました</p>
       <p>taiyo反映されました</p>
-      <img src="./images/book.gif" alt="ノート" style={{ width: '75%' }} /><br/>
+      <p>na2kera反映されました</p>
+      <img src="./images/book.gif" alt="ノート" style={{ width: "75%" }} />
+      <br />
       <p>文字を抽出してノートに保存します。</p>
       <button onClick={onCamera}>カメラの起動</button>
       <button onClick={onAlbum}>アルバム</button>
