6f06618a618d64da50bd699fbc1d03bbee387370
na2kera <natsu@keranatsukinoMacBook-Air.local>
2023-11-04 19:00:20 +0900

    フッターの遷移先を調整しました

--- src/components/parts/footer.jsx
+++ src/components/parts/footer.jsx
@@ -6,25 +6,39 @@ import HomeIcon from "@mui/icons-material/Home";
 import CollectionsBookmarkIcon from "@mui/icons-material/CollectionsBookmark";
 import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import ShareTwoToneIcon from "@mui/icons-material/ShareTwoTone";
 
-export const SimpleBottomNavigation = () => {
+export const SimpleBottomNavigation = ({ onBack, onAlbum, onShare }) => {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: "100%" }}>
       <BottomNavigation
-        //showLabels
-        value={value}
+        showLabels
+        //value={value}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
         <BottomNavigationAction
+          onClick={onBack}
+          label="Home"
+          icon={<HomeIcon />}
+        />
+        <BottomNavigationAction
+          onClick={onAlbum}
           label="Album"
           icon={<CollectionsBookmarkIcon />}
         />
-        <BottomNavigationAction label="Setting" icon={<ManageAccountsIcon />} />
+        <BottomNavigationAction
+          onClick={onShare}
+          label="Share"
+          icon={<ShareTwoToneIcon />}
+        />
+        <BottomNavigationAction
+          label="Setting"
+          icon={<MiscellaneousServicesIcon />}
+        />
       </BottomNavigation>
     </Box>
   );
