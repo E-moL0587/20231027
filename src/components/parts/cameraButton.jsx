f8f5d0dbd4393b6b93eaf6a5ca9ee1be718228af
taiyo <taiyo.for.work@gmail.com>
2023-11-05 14:06:49 +0900

    camera-button

--- src/components/parts/cameraButton.jsx
+++ src/components/parts/cameraButton.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import SvgIcon from "@mui/material/SvgIcon";
 import RadioButtonUncheckedTwoToneIcon from "@mui/icons-material/RadioButtonUncheckedTwoTone";
-import Brightness1Icon from "@mui/icons-material/Brightness1";
+import Brightness1Icon from "@mui/icons-material/Brightness1";;
 
 export const MakeCamerabutton = ({ captureImage }) => {
   return (
     <SvgIcon
       onClick={captureImage}
-      height="10em"
-      width="10em"
       variant="contained"
       color="success"
+      sx={{width:"150px", height:"150px", padding:"110px", left:"42%"}}
     >
-      <Brightness1Icon />
+      <RadioButtonUncheckedTwoToneIcon />
     </SvgIcon>
   );
 };
